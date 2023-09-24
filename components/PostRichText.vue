abad6777a305984c3b53552cb7aecc0d94301de3
pro-soichiro <dr.1987626@gmail.com>
2023-09-24 10:29:58 +0900

    埋め込みブロックエントリーの追加

--- components/PostRichText.vue
+++ components/PostRichText.vue
@@ -26,14 +26,38 @@ export default {
     renderHyperlink({ data, content }, next) {
       return `<a href="${data.uri}" target="_blank">${next(content)}</a>`;
     },
+    renderEmbeddedEntry({data: {target: {fields, sys}}}){
+      if (sys.contentType.sys.id === 'posts') {
+        return `<div class="external-article-widget">
+                  <a href="${fields.slug}" target="_blank">
+                    <strong class="external-article-widget-title">
+                      ${fields.title}
+                    </strong>
+                    <em class="external-article-widget-description">
+                      ${fields.description}
+                    </em>
+                    <em class="external-article-widget-url">
+                      http://localhost:3000/${fields.slug}
+                    </em>
+                  </a>
+                  <a
+                    class="external-article-widget-image"
+                    href="${fields.slug}"
+                    style="background-image: url('${fields.thumbnail.fields.file.url}');"
+                    target="_blank">
+                  </a>
+                </div>
+                `;
+      }
+    },
     richTextResponse(richtextObject) {
       const renderOptions = {
         renderNode: {
+          [BLOCKS.EMBEDDED_ENTRY]: this.renderEmbeddedEntry,
           [BLOCKS.EMBEDDED_ASSET]: this.renderEmbeddedAsset,
           [INLINES.HYPERLINK]: this.renderHyperlink,
         },
       };
-      console.log(renderOptions.renderNode.hyperlink)
       return documentToHtmlString(richtextObject, renderOptions);
     },
   },
@@ -43,5 +67,40 @@ export default {
 <style>
 .post-rich-text img {
   width: 100%;
+}
+.external-article-widget {
+  display: table;
+  width: 100%;
+  border: 1px solid grey;
+  border-radius: 4px;
+}
+.external-article-widget-title {
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+}
+.external-article-widget-description {
+    display: -webkit-box;
+    max-height: 3em;
+    overflow: hidden;
+    word-break: break-all;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+}
+.external-article-widget>a {
+    display: table-cell;
+    padding: 16px;
+    vertical-align: middle;
+}
+.external-article-widget-image {
+    width: 225px;
+    height: 150px;
+    vertical-align: middle;
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: cover;
+    border-left: 1px solid grey;
+    border-radius: 0 3px 3px 0;
 }
 </style>
