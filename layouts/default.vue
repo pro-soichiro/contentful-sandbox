63411693993fce3b5c25a592359cd8b1a76f7a24
pro-soichiro <dr.1987626@gmail.com>
2023-09-24 10:30:10 +0900

    デフォルトレイアウトの追加

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="default">
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  background-color: rgb(240, 235, 235);
+}
+.default {
+  margin: 0 auto;
+  padding: 1rem 2rem;
+  max-width: 620px;
+  background-color: #fff;
+}
+</style>
